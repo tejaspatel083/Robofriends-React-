1086b8d51eb82ff2dc64a80b65dad2700da88acc
TejasPatel <tjp083@gmail.com>
2021-12-09 00:18:00 -0500

    First React Project

---
+++ src/components/SearchBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SearchBox = ({searchfield, searchChange}) =>
+{
+	return(
+
+		<div className='tc pa2'>
+			<input 
+				className='tc pa3 ba b--green bg-lightest-blue'
+				type='search' 
+				placeholder='search robots'
+				onChange={searchChange}
+			/>
+		</div>
+	);
+}
+
+export default SearchBox;
